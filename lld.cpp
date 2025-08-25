9a54ba1e327b7ef43d4e6606fb1e6ce6fbf54cec
Abhilash Rudra <rudraabhilash@users.noreply.github.com>
2025-08-25 19:35:28 +0530

    state

--- lld.cpp
+++ lld.cpp
@@ -35,3 +35,48 @@ int main()
   DivObserver divObs1(&subj, 4);DivObserver divObs2(&subj, 3);ModObserver modObs3(&subj, 3);
   subj.set_val(14);
 }
+
+
+/*
+************************************************************************************************************
+************************************************************************************************************
+*/
+/*state design pattern*/
+interface State {
+    void pull(CeilingFanPullChain wrapper);
+}
+class CeilingFanPullChain {
+    private State currentState;
+    public CeilingFanPullChain() {currentState = new Off();}
+    public void set_state(State s) {currentState = s;}
+    public void pull() {currentState.pull(this);}
+}
+class Off implements State {
+    public void pull(CeilingFanPullChain wrapper) {
+        wrapper.set_state(new Low());System.out.println("low speed");
+    }
+}
+class Low implements State {
+    public void pull(CeilingFanPullChain wrapper) {
+        wrapper.set_state(new Medium());System.out.println("medium speed");
+    }
+}
+class Medium implements State {
+    public void pull(CeilingFanPullChain wrapper) {
+        wrapper.set_state(new High());System.out.println("high speed");
+    }
+}
+class High implements State {
+    public void pull(CeilingFanPullChain wrapper) {
+        wrapper.set_state(new Off());System.out.println("turning off");
+    }
+}
+public class StateDemo {
+    public static void main(String[] args) {
+        CeilingFanPullChain chain = new CeilingFanPullChain();
+        while (true) {
+            System.out.print("Press ENTER");getLine();
+            chain.pull();
+        }
+    }
+}
